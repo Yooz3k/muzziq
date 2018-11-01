5692d5454269e86fcee06b16c97c55a459d0976b
tomaszkononowicz <tomaszkononowicz@gmail.com>
2018-11-01 21:49:21 +0100

    Ability connect multiple client, add MessageTypes for revice

---
+++ Muzziq/Models/WSMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muzziq.Models
+{
+    public enum WSMessageType
+    {
+        TEXT,
+        SCORE,
+        AUDIO,
+        OTHER,
+    }
+    public class WSMessage
+    {
+        public WSMessageType Type { get; set; }
+        public string Text { get; set; }
+    }
+}
--- Muzziq/Services/WSService.cs
+++ Muzziq/Services/WSService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Muzziq.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,12 +11,6 @@ using System.Threading.Tasks;
 
 namespace Muzziq.Services
 {
-    public enum WSMessageType
-    {
-        CONNECT,
-        TEXT,
-        DISCONNECT
-    }
     public interface IWSService
     {
         void SendAll(string message);
@@ -24,37 +19,37 @@ namespace Muzziq.Services
 
     public class WSService : IWSService
     {
-        private IList WebSockets = new List<WebSocket>();
+        private static IDictionary WebSockets = new Dictionary<string, WebSocket>();
+        private const string SEPARATOR = " ";
+        private const int BUFFER_SIZE = 4096;
 
         public async Task Recive(HttpContext context, Func<Task> next)
         {
-            if (context.Request.Path == "/ws1" || context.Request.Path == "/ws2")
+            if (context.WebSockets.IsWebSocketRequest)
             {
-                if (context.WebSockets.IsWebSocketRequest)
+                string id = context.Request.Path.ToString();
+                byte[] buffer;
+                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                WebSockets.Add(id, webSocket);
+
+                WebSocketReceiveResult result = null;
+                do
                 {
-                    byte[] buffer = new byte[1024 * 4];
-                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                    this.WebSockets.Add(webSocket);
-                    //Console.WriteLine(webSocket);
-                    WebSockets.Add(webSocket);
-                    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                    //await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, true, CancellationToken.None);
-                    while (!result.CloseStatus.HasValue)
+                    buffer = new byte[BUFFER_SIZE];
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    string messageBufferSize = Encoding.UTF8.GetString(buffer);
+                    string message = messageBufferSize.Substring(0, messageBufferSize.IndexOf('\0'));
+                    WSMessage wsMessage = new WSMessage
                     {
-                        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                        //await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, true, CancellationToken.None);
-                        string message = Encoding.ASCII.GetString(buffer);
-                        Console.WriteLine(message);
-                        //TODO Dispatch message
-                        this.SendAll(message);
-                    }
-                    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-                    WebSockets.Remove(webSocket);
-                }
-                else
-                {
-                    context.Response.StatusCode = 400;
-                }
+                        Type = getMessageType(message),
+                        Text = getMessageText(message)
+                    };
+                    Console.WriteLine("wsMessage.Type: " + wsMessage.Type);
+                    Console.WriteLine("wsMessage.Text: " + wsMessage.Text);
+                    Dispatch(wsMessage);
+                } while (result != null && !result.CloseStatus.HasValue);
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                WebSockets.Remove(id);
             }
             else
             {
@@ -68,6 +63,43 @@ namespace Muzziq.Services
             {
                 byte[] bytes = Encoding.ASCII.GetBytes(message);
                 await webSocket.SendAsync(new ArraySegment<byte>(bytes, 0, bytes.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+        private void Dispatch(WSMessage message)
+        {
+            switch (message.Type)
+            {
+                case WSMessageType.TEXT: break;
+                case WSMessageType.SCORE: break;
+                case WSMessageType.AUDIO: break;
+                case WSMessageType.OTHER: break;
+            }
+        }
+
+        private WSMessageType getMessageType(string message)
+        {
+            string firstWord = message.Split(SEPARATOR)[0];
+            if (Enum.TryParse(firstWord, out WSMessageType messageType))
+            {
+                return messageType;
+            }
+            else
+            {
+                return WSMessageType.OTHER;
+            }
+        }
+
+        private string getMessageText(string message)
+        {
+            try
+            {
+                string text = message.Substring(message.Split(SEPARATOR)[0].Length);
+                return text.Trim();
+            }
+            catch
+            {
+                return string.Empty;
             }
         }
     }
