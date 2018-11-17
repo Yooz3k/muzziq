a9ec3c7169a54fcb69f360708d25a5d911e5bd9a
jaskier07 <aleksanderkania96@gmail.com>
2018-11-17 23:15:13 +0100

    Add new song view (without file uploading)

---
+++ Muzziq/Controllers/SongController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Muzziq.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult AddNewSongView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewSong(String title, String author, String album, String genre, String year)
+        {
+            // TODO logika dodania nowej piosenki do bazy danych
+
+            return View("AddNewSongView");
+        }
+    }
+}
