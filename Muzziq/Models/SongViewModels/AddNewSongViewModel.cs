e24b61d85a72e06325c5205f08899d41bcf8a303
jaskier07 <aleksanderkania96@gmail.com>
2018-11-25 10:58:34 +0100

    Sending songs in browser form, validating them and saving to database

--- Muzziq/Models/SongViewModels/AddNewSongViewModel.cs
+++ Muzziq/Models/SongViewModels/AddNewSongViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +9,23 @@ namespace Muzziq.Models.SongViewModels
 {
     public class AddNewSongViewModel
     {
-        //TODO: pola wraz z getterami i setterami
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String Album { get; set; }
+        public String Genre { get; set; }
+        public int Year { get; set; }
+        public IFormFile File { get; set; }
+        public String Info { get; set; }
+
+        public AddNewSongViewModel()
+        {
+            this.Title = "";
+            this.Author = "";
+            this.Album = "";
+            this.Genre = "";
+            this.Year = 2018;
+            this.File = null;
+            this.Info = "";
+        }
     }
 }
